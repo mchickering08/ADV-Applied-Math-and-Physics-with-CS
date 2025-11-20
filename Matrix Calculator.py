f463b35b96ed6c91c6b9e2d7424403cf3d6887b4
mchickering08 <72114543+mchickering08@users.noreply.github.com>
2025-11-20 09:23:07 -0500

    update

--- Matrix Calculator.py
+++ Matrix Calculator.py
@@ -1,1 +1,7 @@
-#test
+'''
+Matrix: two dim ray of numbers, m rows and n columns, entries are real numbers
+
+Base Assignment
+    Matrix class must have print, plus, times, scalarTimesRow, switchRows, linearCombRos, rowreduce, and invert
+
+'''
