352271c817a9b25b616cdf162daf50722265afa1
mchickering08 <72114543+mchickering08@users.noreply.github.com>
2025-11-22 16:25:22 -0500

    create matrix

--- Matrix_Calculator.py
+++ Matrix_Calculator.py
@@ -1,37 +1,73 @@
 '''
-Matrix: two dim ray of numbers, m rows and n columns, entries are real numbers
-
-Base Assignment
-    Matrix class must have print, plus, times, scalarTimesRow, switchRows, linearCombRos, rowreduce, and invert
-
-'''
-
+test
 mat = [[1, 2, 3, 4],
        [5, 6, 7, 8],
        [9, 10, 11, 12]]
 
 print(mat)
 
-m_rows = int(input("Input the number of rows: "))
-n_col = int(input("Input the number of columns: "))
 
-matrix = []
-print("Input one row at a time (seperate w/ spaces)")
+  print("\n2D matrix is:")
+    for i in range(m_rows):
+        for j in range(n_col):
+            print(matrix[i][j], end=" ")
+        print()
+'''
 
-for i in range(m_rows):   
-    row_input = input("Enter row " + str(i+1) + ": ")
-    row = list(map(int, row_input.split()))
+class Matrix:
+    def __init__(self, name, value): #initialize
+        self.name = name
+        self.value = value
+        self.rows = len(value)
+        self.cols = len(value[0]) #checks columns in matrix
 
-    while len(row) != n_col:
-        print(f"Please enter exactly " + str(n_col) + " values.")
-        row_input = input(f"Enter row {i+1}: ")
+    #print
+
+    #add - example trixie.plus(alice)
+
+    #multiply
+
+    #times scalar row
+
+    #switch rows
+def check_int(prompt):
+    while True:
+        value = input(prompt)
+        if value.isdigit():
+            return int(value)
+        else:
+            print("Invalid input. Please enter an integer")
+
+def get_matrix_input():
+    name = input("Matrix name: ")
+    m_rows = check_int("Enter number of rows: ")
+    n_col = check_int("Enter number of columns: ")
+
+    value = []
+    print("Input one row at a time (seperate w/ spaces)") #add check for other characters
+
+    for i in range(m_rows):   
+        row_input = input("Enter row " + str(i+1) + ": ")
         row = list(map(int, row_input.split()))
 
-    matrix.append(row)
+        while len(row) != n_col:
+            print(f"Please enter exactly [" + str(n_col) + "] values.")
+            row_input = input("Enter row " + str(i+1) + ": ")
+            row = list(map(int, row_input.split()))
 
+        value.append(row)
+    
+    return Matrix(name, value)
 
-print("\n2D matrix is:")
-for i in range(m_rows):
-    for j in range(n_col):
-        print(matrix[i][j], end=" ")
-    print()
+if __name__ == "__main__":
+
+    print("Create your matrix: ")
+    m = get_matrix_input() #usrr input
+
+    print("\nMatrix created:")
+    print(m.name)
+    for i in range(m.rows):
+        for j in range(m.cols):
+            print(m.value[i][j], end=" ")
+        print()
+    print("Rows:" + str(m.rows) + "\nColums:" + str(m.cols))
